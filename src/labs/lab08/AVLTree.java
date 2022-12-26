84cd2e72fcf2aa4f309aa458ae3b2dff516c2030
nimrod46 <nimrodma99@gmail.com>
2022-12-26 14:41:09 +0200

    passing testSingleRotateRight

--- src/labs/lab08/AVLTree.java
+++ src/labs/lab08/AVLTree.java
@@ -15,32 +15,32 @@ public class AVLTree<T extends Comparable<T>> {
     //add the value to the tree, and return the updated root of the tree.
     public AVLTree<T> add(T value) {
         if (value.compareTo(this.value) < 0) {
-            if (right != null) {
-                right = right.add(value);
-                height = Math.max(right.height, left != null ? left.height : -1) + 1;
-                if (getBalanceFactor() == -2) {
-                    if (right.getBalanceFactor() <= 0) {
-                        AVLTree<T> r = right;
-                        AVLTree<T> tmp = right.left;
-                        right.left = this;
-                        this.right = tmp;
+            if (left != null) {
+                left = left.add(value);
+                height = Math.max(left.height, right != null ? right.height : -1) + 1;
+                if (getBalanceFactor() == 2) {
+                    if (left.getBalanceFactor() >= 0) {
+                        AVLTree<T> r = left;
+                        AVLTree<T> tmp = left.right;
+                        left.right = this;
+                        this.left = tmp;
                         return r;
                     }
                 }
                 return this;
             }
-            right = new AVLTree<>(value);
-            height = Math.max(right.height, left != null ? left.height : -1) + 1;
-            return this;
-        }
-
-        if (left != null) {
-            left = left.add(value);
+            left = new AVLTree<>(value);
             height = Math.max(left.height, right != null ? right.height : -1) + 1;
             return this;
         }
-        left = new AVLTree<>(value);
-        height = Math.max(left.height, right != null ? right.height : -1) + 1;
+
+        if (right != null) {
+            right = right.add(value);
+            height = Math.max(right.height, left != null ? left.height : -1) + 1;
+            return this;
+        }
+        right = new AVLTree<>(value);
+        height = Math.max(right.height, left != null ? left.height : -1) + 1;
         return this;
     }
 
