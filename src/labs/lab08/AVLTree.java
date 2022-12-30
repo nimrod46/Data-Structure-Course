63d42c27ddd677e8a8fb38911a0aebacc983c375
nimrod46 <nimrodma99@gmail.com>
2022-12-30 15:21:45 +0200

    Finished lab08

--- src/labs/lab08/AVLTree.java
+++ src/labs/lab08/AVLTree.java
@@ -17,37 +17,34 @@ public class AVLTree<T extends Comparable<T>> {
         if (value.compareTo(this.value) < 0) {
             if (left != null) {
                 left = left.add(value);
-                updateHight();
+                updateHeight();
                 if (getBalanceFactor() == 2) {
                     if (left.getBalanceFactor() >= 0) {
-                        AVLTree<T> r = left;
-                        AVLTree<T> tmp = left.right;
-                        left.right = this;
-                        this.left = tmp;
-                        updateHight();
-                        r.updateHight();
-                        return r;
+                        return leftRotate(this);
                     }
+                    return leftRightRotate(this);
                 }
                 return this;
             }
             left = new AVLTree<>(value);
-            updateHight();
+            updateHeight();
             return this;
         }
 
         if (right != null) {
             right = right.add(value);
-            updateHight();
+            updateHeight();
+            if (getBalanceFactor() == -2) {
+                if (right.getBalanceFactor() <= 0) {
+                    return rightRotate(this);
+                }
+                return rightLeftRotate(this);
+            }
             return this;
         }
         right = new AVLTree<>(value);
-        updateHight();
+        updateHeight();
         return this;
-    }
-
-    private void updateHight() {
-        height = Math.max(left != null ? left.height : -1, right != null ? right.height : -1) + 1;
     }
 
     //return the value in this node
@@ -65,10 +62,53 @@ public class AVLTree<T extends Comparable<T>> {
         return right;
     }
 
+    private void updateHeight() {
+        height = Math.max(left != null ? left.height : -1, right != null ? right.height : -1) + 1;
+    }
+
     private int getBalanceFactor() {
         int leftHeight = left != null ? left.height : -1;
         int rightHeight = right != null ? right.height : -1;
         return leftHeight - rightHeight;
     }
 
+    private static <Y extends Comparable<Y>> AVLTree<Y> leftRightRotate(AVLTree<Y> head) {
+        AVLTree<Y> left = head.left;
+        AVLTree<Y> leftRight = head.left.right;
+        left.right = leftRight.left;
+        leftRight.left = left;
+        head.left = leftRight;
+        return leftRotate(head);
+    }
+
+    private static <Y extends Comparable<Y>> AVLTree<Y> leftRotate(AVLTree<Y> head) {
+        AVLTree<Y> newHead = head.left;
+        AVLTree<Y> tmp = head.left.right;
+        head.left.right = head;
+        head.left = tmp;
+        newHead.right.updateHeight();
+        newHead.left.updateHeight();
+        newHead.updateHeight();
+        return newHead;
+    }
+
+    private static <Y extends Comparable<Y>> AVLTree<Y> rightLeftRotate(AVLTree<Y> head) {
+        AVLTree<Y> right = head.right;
+        AVLTree<Y> rightLeft = head.right.left;
+        right.left = rightLeft.right;
+        rightLeft.right = right;
+        head.right = rightLeft;
+        return rightRotate(head);
+    }
+
+    private static <Y extends Comparable<Y>> AVLTree<Y> rightRotate(AVLTree<Y> head) {
+        AVLTree<Y> newHead = head.right;
+        AVLTree<Y> tmp = head.right.left;
+        head.right.left = head;
+        head.right = tmp;
+        newHead.left.updateHeight();
+        newHead.right.updateHeight();
+        newHead.updateHeight();
+        return newHead;
+    }
 }
